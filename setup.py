3df9b1fe7d0dc9330ff182452635f9fd2144dfb2
Alex Liberzon <alex.liberzon@gmail.com>
2020-08-01 23:33:36 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bloodtrack',
+      version='0.0.1',
+      description='Open source toolkit for Lagrangian tracking and blood damage analysis',
+      url='http://github.com/openptv/bloodtrack',
+      author='OpenPTV consortium',
+      author_email='alex.liberzon@gmail.com',
+      license='LGPL',
+      packages=['bloodtrack'],
+      zip_safe=False)
